a58ce5b93421601ab90e186989e7ddd45624e7b2
Maycon Yamamotto <mayconyamamotto54@gmail.com>
2023-09-17 09:56:07 -0300

    Ajustes

---
+++ script.js
@@ -0,0 +1,33 @@
+!(function (exports) {
+    exports.submitGoogleForm = submitGoogleForm;
+  
+    function submitGoogleForm(form) {
+      try {
+        var data = [].slice
+          .call(form)
+          .map(function (control) {
+            return "value" in control && control.name
+              ? control.name +
+                  "=" +
+                  (control.value === undefined ? "" : control.value)
+              : "";
+          })
+          .join("&");
+        var xhr = new XMLHttpRequest();
+        console.log(form.action);
+  
+        xhr.open("POST", form.action + "/formResponse", true);
+        xhr.setRequestHeader("Accept", "application/xml, text/xml, */*; q=0.01");
+        xhr.setRequestHeader(
+          "Content-type",
+          "application/x-www-form-urlencoded; charset=UTF-8"
+        );
+        xhr.send(data);
+      } catch (e) {}
+  
+      form.parentNode.className += " submitted";
+      alert("Salvo com sucesso!");
+      form.reset();
+      return false;
+    }
+  })(typeof module === "undefined" ? window : module.exports);
